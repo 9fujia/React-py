675e281d5fb972d68d83f7e8cb086c0856d38b3c
zjhlaap <zjhlaap@163.com>
2019-04-12 11:12:50 +0800

    zzz

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-//全局资源配置
-// import './assets/js/rem';
-// import './assets/css/base.css';
-// import './assets/css/page.css';
-// import './assets/css/all.css'
-// import './assets/css/mui.min.css';
-// import './assets/css/loaders.min.css';
-// import './assets/css/loading.css';
-// import './assets/slick/slick';
-
 
 //路由配置
-
 import { BrowserRouter,Route} from 'react-router-dom'
 import {Provider} from 'react-redux';
 import App from "./components/App";
